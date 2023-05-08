471650df7bf13a0a39610b5bb84fe5a8963a0e9f
koramit <koramit.p@gmail.com>
2023-05-08 15:25:42 +0700

    built

---
+++ public/build/assets/RegisterForm.6c50e9c1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,c as t}from"./app.4afe38cf.js";const o={name:"RegisterForm"};function s(c,n,a,_,p,i){return r(),t("h1",null,"register")}const f=e(o,[["render",s]]);export{f as default};
