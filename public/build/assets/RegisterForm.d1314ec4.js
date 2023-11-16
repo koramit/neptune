37af0ad022ca391c59411d1d8bfaf6a7c8d57073
koramit <koramit.p@gmail.com>
2023-11-16 23:15:41 +0700

    build

--- public/build/assets/RegisterForm.d1314ec4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c as t}from"./app.7720fea2.js";const o={name:"RegisterForm"};function s(c,n,a,_,p,i){return r(),t("h1",null,"register")}const f=e(o,[["render",s]]);export{f as default};
